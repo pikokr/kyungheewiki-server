06bf6fc0acbbd8025ca770e6d5ee39e6779c8882
pikokr <parangee9706@gmail.com>
2022-09-11 21:59:46 +0900

    feat: /me endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import type { FastifyPluginAsync } from 'fastify'
 import { ZodError } from 'zod'
 import { logger } from '~/logger'
+import { UnauthorizedError, getUser } from '~/utils/users'
 
 import { authRoutes } from './auth'
 
@@ -10,6 +11,12 @@ export const routes: FastifyPluginAsync = async (server) => {
       return reply.status(400).send({
         type: 'validationError',
         issues: err.issues,
+      })
+
+    if (err instanceof UnauthorizedError)
+      return reply.status(401).send({
+        type: 'unauthorized',
+        message: 'Unauthorized',
       })
 
     logger.error(err)
@@ -22,6 +29,12 @@ export const routes: FastifyPluginAsync = async (server) => {
 
   server.get('/', async () => {
     return { hello: 'world' }
+  })
+
+  server.get('/me', async (req) => {
+    const user = (await getUser(req))!
+
+    return user.toAPIUser()
   })
 
   server.register(authRoutes, { prefix: '/auth' })
