a2c866ac3818676d32830c284d2e7c316fb7f66c
pikokr <parangee9706@gmail.com>
2022-09-11 19:09:40 +0900

    chore: connect to database

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import chalk from 'chalk'
 import 'dotenv/config'
 import fastify from 'fastify'
+import mongoose from 'mongoose'
 
 import { logger } from './logger'
+import './models'
+
+process.on('unhandledRejection', (err) => {
+  logger.fatal('Unhandled rejection', err)
+})
+
+process.on('uncaughtException', (err) => {
+  logger.fatal('Uncaught exception', err)
+})
 
 const server = fastify()
 
 setImmediate(async () => {
+  await mongoose.connect(process.env.DB_DSN!)
+
+  logger.info('Connected to database.')
+
   const addr = await server.listen({
     port: +(process.env.PORT || 3000),
     host: process.env.HOST || '0.0.0.0',
